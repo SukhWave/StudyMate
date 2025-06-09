85c8e1cadfef1f96cf80c7338438f11b69a88367
Your Name <you@example.com>
2025-06-09 08:01:14 -0400

    edited css

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <title>StudyMate</title>
-    <link rel="stylesheet" href="assets/style.css" />
+    <link rel="stylesheet" href="css/main.css" />
 </head>
 <body>
 <header>
